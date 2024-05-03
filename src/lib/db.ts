0ca4c0db6a5d3602f51bc40b062fa4495e24b886
Nico J <nico.javelaud@proton.me>
2024-05-02 22:35:15 -0400

    Initial commit

---
+++ src/lib/db.ts
@@ -0,0 +1,7 @@
+
+import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$env/static/private';
+
+// Create a single supabase client for interacting with your database
+export const supabase = createClient( SUPABASE_URL, SUPABASE_ANON_KEY );
+
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import "@src/app.css";
+  </script>
+  <svelte:head>
+    <!-- <script defer data-domain="xxx.com" src="https://plausible.io/js/script.tagged-events.js"></script> -->
+  </svelte:head>
+  
+  <slot />
